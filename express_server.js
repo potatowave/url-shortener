8c62ae504f286b55f772a80a48f199f6ac0a6651
Mike Jenkins <kinetic@icloud.com>
2016-10-14 02:45:29 +0000

    reinstated body parser

--- express_server.js
+++ express_server.js
@@ -6,6 +6,8 @@ const app = express();
 const Cookies = require('cookies');
 const KeyGrip = require("keygrip")
 const keys = new KeyGrip(['samsung note 7', 'iphone 7'], 'sha256')
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
 
 // Configuration
 
@@ -15,33 +17,61 @@ app.set('view engine', 'ejs'); // Set View Engine to ejs
 // Run time data
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+            "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userid: "3k3k33" },
+            "9sm5xK": { longURL: "http://www.google.com", userid: "3k3k33" }   };
+
+var users = { "3k3k33": { id: "3k3k33",  email: "kinetic@icloud.com", password: "lighthouse"} };
 
 // Middleware
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
-
 app.use((req, res, next) => {
-  req.cookies = new Cookies( req, res, { "keys": keys } )
+  req.cookies = new Cookies( req, res, { "keys": keys } );
   next();
-})
+
+
+});
 
 // Routing
 
 // respond to gets from /urls
 
-app.get("/urls/new", (req, res) => {
-  let templateVars = { shortURL: req.params.id, username: req.cookies.get("username"), urlDatabase};
-  res.render("urls_new", templateVars);
+app.post("/urls/register", (req, res) => {
+
+  const id =  String(randStr());
+  console.log(JSON.stringify(req.body));
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (!email) {
+    res.sendStatus(400);
+    return;
+  }
+
+  if (!password) {
+    res.sendStatus(400);
+    return;
+  } else {
+
+
+    users[id] = { id: id, email: email , password: password };
+    req.cookies.set("userid", id);
+    res.redirect('urls/');
+
+  }
+
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, username: req.cookies.get("username"), urlDatabase};
-  console.log(req.params.id);
-  res.render("urls_view", templateVars);
+
+app.get("/urls/register", (req, res) => {
+  let templateVars = { urls: urlDatabase, userid: req.cookies.get("userid") }
+  res.render("urls_register", templateVars);
+});
+
+
+
+app.get("/urls/new", (req, res) => {
+  let templateVars = { shortURL: req.params.id, userid: req.cookies.get("userid"),  urls: urlDatabase};
+  res.render("urls_new", templateVars);
 });
 
 // respond to gets from /urls/new (serves form)
@@ -49,7 +79,7 @@ app.get("/urls/:id", (req, res) => {
 // resond to posts from /urls
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies.get("username") }
+  let templateVars = { urls: urlDatabase, userid: req.cookies.get("userid") }
   res.render("urls_index", templateVars);
 });
 
@@ -57,14 +87,14 @@ app.post("/urls", (req, res) => {
   // console.log(req.body.longURL);  // debug statement to see POST parameters
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
   const tinyString =  String(randStr());
-  urlDatabase[tinyString] = req.body.longURL;
+  urlDatabase[tinyString] = { longURL:req.body.longURL, userid: req.cookies.get("userid")};
   console.log(urlDatabase);
   res.redirect('urls/' + tinyString);
 
 });
 
 app.get("/url/:shortURL", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies.get() }
+  let templateVars = { urls: urlDatabase, userid: req.cookies.get() }
   res.render("urls_view", templateVars);
 
 });
@@ -82,7 +112,7 @@ app.get("/u/:shortURL", (req, res) => {
 
 });
 
-app.post("/login", (req, res) => {
+app.post("/urls/login", (req, res) => {
 
   let userName = req.body.username;
   req.cookies.set("username", userName);
@@ -91,9 +121,9 @@ app.post("/login", (req, res) => {
 
 });
 
-app.post("/logout", (req, res) => {
+app.post("/urls/logout", (req, res) => {
 
-  res.clearCookie("username");
+  res.clearCookie("userid");
   res.redirect("/urls/");
 
 });
@@ -116,6 +146,13 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(301, /urls/);
 
 });
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, userid: req.cookies.get("userid"), urls: urlDatabase};
+  console.log(req.params.id);
+  res.render("urls_view", templateVars);
+});
+
 
 // Tell the console the server is running
 
